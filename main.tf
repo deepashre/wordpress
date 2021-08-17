4c9f24627bd0078503fa35cd9514220c36191a9f
deepashre <86648444+deepashre@users.noreply.github.com>
2021-08-17 11:31:01 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = var.aws_region
+}
+resource "aws_instance" "wp" { 
+    ami = "ami-0adef6268736a8a10"
+    instance_type = "t2.micro"
+    key_name = "cloud-poc"
+    
+    tags = {
+    Name = "wp-app"
+    }
+
+}
+output "publicip" {
+  value = aws_instance.wp.public_ip
+}
